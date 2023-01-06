6f32d8dda07722e6a50bcc06661d7d1c061d079d
Muhammad Rafay <muhammadrafay7655@gmail.com>
2023-01-06 23:22:32 +0500

    Inprogress

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -9,7 +16,7 @@ def home_page():
         "Welcome": "To Home Page"
     }
 
-@app.post("/login")
-def login_user(username, password):
-    return (username,password)
-    
+
+@app.post("/create_post")
+def create_post(post: Post):
+    print(post)
